081ffcb2a187c8353ddbdb2527078a391728e1b2
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-03-01 00:40:21 +0100

    added oath library

--- potato_blog_ric/models.py
+++ potato_blog_ric/models.py
@@ -1,18 +1,34 @@
 from django.db import models
 import scaffold.settings as settings
 
+def PredictLanguage(message):
+  return "English"
+
+def GetSentiment(message):
+    """
+    TODO return true or false based on a predictive analysis using Google Prediction API
+    :param message:
+    :return:
+    """
+    return True
+
 class BlogArticle(models.Model):
     title = models.CharField(max_length=60)
     text = models.TextField()
     blog_content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    sentiment = models.BooleanField()
+
     class Meta:
         get_latest_by = "created_on"
         ordering = ["-created_on"]
 
     def __unicode__(self):
         return self.title
+
+    def setSentiment(self):
+        self.sentiment = GetSentiment(self.blog_content)
 
 class ImageBlogArticle(models.Model):
     title = models.CharField(max_length=60)
